f2d10f949eec17eab72fdcc9362104f8111180fe
marinasantosa95-dotcom <marina.santosa95@gmail.com>
2026-05-19 16:10:39 -0600

    Update LoginPage.java
    Added code on login

--- automation-frameworks/java-testng/src/main/java/com/portfolio/pages/LoginPage.java
+++ automation-frameworks/java-testng/src/main/java/com/portfolio/pages/LoginPage.java
@@ -1,1 +1,26 @@
+package com.portfolio.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    private final By usernameField = By.id("username");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.cssSelector("button[type='submit']");
+    private final By flashMessage = By.id("flash");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+        type(usernameField, username);
+        type(passwordField, password);
+        click(loginButton);
+    }
+
+    public String getAlertMessage() {
+        return getText(flashMessage);
+    }
+}
