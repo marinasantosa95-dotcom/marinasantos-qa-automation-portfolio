09fdde6e5a24e6190b4780f4e74384f3db775a8c
marinasantosa95-dotcom <marina.santosa95@gmail.com>
2026-05-19 16:12:11 -0600

    Update LoginTest.java

--- automation-frameworks/java-testng/src/test/java/com/portfolio/tests/LoginTest.java
+++ automation-frameworks/java-testng/src/test/java/com/portfolio/tests/LoginTest.java
@@ -1,1 +1,21 @@
+package com.portfolio.tests;
 
+import com.portfolio.base.BaseTest;
+import com.portfolio.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test(description = "Validar el inicio de sesión con credenciales correctas")
+    public void testLoginExitoso() {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("tomsmith", "SuperSecretPassword!");
+
+        String mensajeEsperado = "You logged into a secure area!";
+        Assert.assertTrue(loginPage.getAlertMessage().contains(mensajeEsperado), 
+                "El mensaje de éxito no se encuentra en la pantalla.");
+    }
+}
